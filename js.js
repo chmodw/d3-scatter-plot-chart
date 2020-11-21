09fd4035f292e5039b64c247eba5966b9f6c189d
chamodya wimansha <chamodyawimansha@gmail.com>
2020-11-21 19:35:23 +0530

    tooltip works

--- js.js
+++ js.js
@@ -31,15 +31,13 @@ d3.json(
       .attr("transform", "translate(60, 450)");
 
     // Adding Y axis
-    let time = data.map((item) => {
-      let mins = toMinutes(item.Seconds);
-      return new Date(Date.UTC(1995, 5, 3, 0, mins[0], mins[1]));
-    });
-
     let yScale = d3
       .scaleTime()
       .range([0, height])
-      .domain([d3.min(time), d3.max(time)]);
+      .domain([
+        d3.min(data.map((item) => timeParser(item.Seconds))),
+        d3.max(data.map((item) => timeParser(item.Seconds))),
+      ]);
 
     svg
       .append("g")
@@ -47,26 +45,56 @@ d3.json(
       .attr("id", "y-axis")
       .attr("transform", "translate(60, 50)");
 
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
+
     // Adding Dots
     svg
       .append("g")
-      .selectAll("dot")
+      .selectAll(".dot")
       .data(data)
       .enter()
       .append("circle")
+      .attr("class", "dot")
       .attr("cx", (d) => xScale(d.Year) + 60)
-      .attr("cy", (d, i) => yScale(time[i]) + 50)
-      .attr("r", 5)
-      .style("fill", "#69b3a2");
+      .attr("cy", (d) => yScale(timeParser(d.Seconds)) + 50)
+      .attr("r", 7)
+      .style("stroke", "black")
+      .style("fill", (d) => color(d.Doping !== ""))
+      .style("opacity", 0.7)
+      .on("mouseover", (e, d) => {
+        d3.select("#tooltip")
+          .style("opacity", 0.9)
+          .style("top", yScale(timeParser(d.Seconds)) + 100 + "px")
+          .style("left", xScale(d.Year) + 130 + "px");
+        d3.select("#tooltip-name").text(d.Name);
+        d3.select("#tooltip-country").text(d.Nationality);
+        d3.select("#tooltip-year").text("Year : " + d.Year);
+        d3.select("#tooltip-time").text("Time : " + d.Time);
+        d3.select("#tooltip-allegation").text(d.Doping);
+      })
+      .on("mouseout", function () {
+        d3.select("#tooltip").style("opacity", 0);
+      });
+
+    // adding label to the y axis
+    svg
+      .append("text")
+      .attr("id", "y-axis-label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", -2)
+      .attr("x", 0 - height / 2)
+      .attr("dy", "1.2em")
+      .style("text-anchor", "middle")
+      .text("Time in Minutes");
   })
   .catch((error) => {
     if (error) throw error;
   });
 
 //convert seconds to minutes -> [mm,ss]
-function toMinutes(seconds) {
+function timeParser(seconds) {
   let secs = seconds % 60;
   let mins = (seconds - secs) / 60;
 
-  return [mins, secs];
+  return new Date(Date.UTC(1995, 5, 3, 0, mins, secs));
 }
